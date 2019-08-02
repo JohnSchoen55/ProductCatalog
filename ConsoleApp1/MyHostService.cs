86ddc1c678b565e4887d691d1556c29bec97d489
John Schoen <john.schoen@spreetail.com>
2019-08-02 15:14:44 -0500

    Add project files.

---
+++ ConsoleApp1/MyHostService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyHostService : IHostedService
+    {
+        public ISayThings SayThings { get; }
+
+        public MyHostService(ISayThings sayThings)
+            {
+
+            sayThings = SayThings;
+
+            }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine(SayThings.saySomething());
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine(SayThings.saySomething());
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder();
+            hostBuilder.ConfigureAppConfiguration(configBuilder =>
+            {
+               
+
+            });
+
+            hostBuilder.ConfigureServices(collection =>
+            {
+                collection.AddHostedService<MyHostService>();
+                collection.AddTransient<ISayThings, SayThings>();
+                collection.AddTransient<ISayThings, SayStuff>();
+
+            });
+
+            var host = hostBuilder.Build();
+            host.Run();
+
+
+        }
+    }
+
+    public interface ISayThings
+    {
+        string saySomething();
+
+        string sayStuff();
+    }
+
+    public class SayThings : ISayThings
+    {
+        public string saySomething()
+        {
+            throw new System.NotImplementedException();
+
+        }
+
+        public string sayStuff()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class SayStuff : ISayThings
+    {
+        SayStuff
+        
+
+        public string sayStuff()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
